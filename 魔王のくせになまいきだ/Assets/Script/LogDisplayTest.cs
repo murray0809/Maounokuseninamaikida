a1d85a6d826c02c47f42f660d10391d73f302bcd
murray0809 <tomo19940809@gmail.com>
2019-11-27 22:30:22 +0900

    log test

---
+++ 魔王のくせになまいきだ/Assets/Script/LogDisplayTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogDisplayTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+            print("Zを押しました");
+        if (Input.GetKeyDown(KeyCode.X))
+            Debug.Log("Xを押しました");
+        if (Input.GetKeyDown(KeyCode.C))
+            Debug.LogWarning("Cを押しました");
+    }
+}
