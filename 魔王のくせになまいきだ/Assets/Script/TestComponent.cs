3e47300ea8a7e78c83c7486ea2470d474b42e473
murray0809 <tomo19940809@gmail.com>
2019-12-06 00:21:45 +0900

    log

---
+++ 魔王のくせになまいきだ/Assets/Script/TestComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestComponent : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            Debug.LogFormat("Time:{0}", System.DateTime.Now);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+} // class TestComponent
