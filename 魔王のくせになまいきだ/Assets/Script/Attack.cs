ec0f90e89ea24c6569f8b7ee19bf80b68108774a
murray0809 <tomo19940809@gmail.com>
2019-12-17 00:38:22 +0900

    attack

--- 魔王のくせになまいきだ/Assets/Script/Attack.cs
+++ 魔王のくせになまいきだ/Assets/Script/Attack.cs
@@ -12,8 +12,6 @@ public class Attack : MonoBehaviour
 
     [SerializeField] public Text m_Text = default;
 
-    [SerializeField] Text m_messageText = default;
-
     public PlayerController playerController;
     public EnemyController enemyController;
 
