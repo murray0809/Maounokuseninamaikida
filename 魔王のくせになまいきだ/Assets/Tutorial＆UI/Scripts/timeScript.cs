5f1fdcaf21df5eab51a4b028c434437774b05ba0
kagerou772 <kagerou772@gmail.com>
2020-01-29 01:09:01 +0900

    制限時間等の追加

---
+++ 魔王のくせになまいきだ/Assets/Tutorial＆UI/Scripts/timeScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timeScript : MonoBehaviour
+{
+    GameObject limitTimeUI;
+    float time = 300.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        limitTimeUI = GameObject.Find("LimitTimeUI");
+    }
+
+    void Update()
+    {
+        // 毎フレーム毎に残り時間を減らしていく
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            limitTimeUI.GetComponent<Text>().text = "終了";
+        }
+        else
+        {
+            // timeを文字列に変換したものをテキストに表示する
+            // ToStringのF1とは、小数点以下1桁までという意味
+            limitTimeUI.GetComponent<Text>().text = time.ToString("F1");
+        }
+    }
+}
