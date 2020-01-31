46839225568d0513978d52806d5c6f351af9eb6b
murray0809 <tomo19940809@gmail.com>
2020-01-31 22:46:49 +0900

    gameover

--- 魔王のくせになまいきだ/Assets/Script/Timer.cs
+++ 魔王のくせになまいきだ/Assets/Script/Timer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class Timer : MonoBehaviour
     bool m_isWorking; // 動作中フラグ
     [SerializeField] float m_timer; //タイマー
 
-    private void Start()
+    void Start()
     {
         m_isWorking = true;
     }
@@ -19,11 +20,21 @@ public class Timer : MonoBehaviour
         {
             m_timer -= Time.deltaTime;
             Refresh();
+
+            if (m_timer <= 0)
+            {
+                GameOver();
+            }
         }
     }
 
     void Refresh()
     {
         m_console.text = m_timer.ToString("F2");    // 小数点以下２桁を表示する
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
     }
 }
